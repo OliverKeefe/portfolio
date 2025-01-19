dccadedb2ce07ad4a274e01d31cb034fd2963437
OliverKeefe <oliverkeefe@tutanota.com>
2025-01-19 19:42:36 +0000

    Modified router class

--- src/router/Router.ts
+++ src/router/Router.ts
@@ -43,7 +43,7 @@ export class Router {
         }
     }
 
-    private getCurrentPath(): string {
+    protected getCurrentPath(): string {
         return window.location.hash.slice(1) || "/"
     }
 }
